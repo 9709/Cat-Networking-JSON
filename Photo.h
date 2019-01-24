0652df46f87337bf2c92ba2f7b9eb1b42f2af4c1
Matthew Chan <mat_t4@hotmail.com>
2019-01-24 14:06:40 -0800

    Created

---
+++ Photo.h
@@ -0,0 +1,17 @@
+//
+//  Photo.h
+//  Cats-Networking-JSON
+//
+//  Created by Matthew Chan on 2019-01-24.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Photo : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Photo.m
@@ -0,0 +1,13 @@
+//
+//  Photo.m
+//  Cats-Networking-JSON
+//
+//  Created by Matthew Chan on 2019-01-24.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import "Photo.h"
+
+@implementation Photo
+
+@end
